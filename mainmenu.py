0a4293d3c65ec84dce96e011a48b52f4d1abc334
raresrusu38 <rares.rusu29@gmail.com>
2020-05-03 16:57:44 +0300

    finished mainmenu window

--- mainmenu.py
+++ mainmenu.py
@@ -1,15 +1,18 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
 from static import style
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        size = (400,400)
         self.setWindowTitle("Manage Employees")
         self.setWindowIcon(QIcon("icons/icon.ico"))
-        self.setGeometry(470, 200, 350,400)
+        self.setGeometry(470, 200, 400,400)
         self.setStyleSheet(style.mainWindowStyle())
+        self.setFixedSize(self.size())
         self.UI()
         self.show()
 
@@ -18,16 +21,54 @@ class MainWindow(QMainWindow):
         self.layouts()
 
     def widgets(self):
-        pass
+        ###################################################################################
+        ### Creating Widgets                    
+        ###################################################################################
+        self.emptyWidgetTop = QLabel()
+        self.manageEmployeesBtn = QPushButton("Manage Employees")
+        self.manageEmployeesBtn.clicked.connect(self.manageEmployee)
+        self.manageEmployeesBtn.setStyleSheet(style.manageEmployeesBtnStyle())
+        self.viewChartsBtn = QPushButton("View Charts")
+        self.viewChartsBtn.clicked.connect(self.viewCharts)
+        self.viewChartsBtn.setStyleSheet(style.viewChartsBtnStyle())
+        self.emptyWidgetBottom = QLabel()
 
     def layouts(self):
+        ###################################################################################
+        ### Creating Layouts                    
+        ###################################################################################
         self.mainLayout         = QVBoxLayout()
-        self.topLayout          = QVBoxLayout()
+        self.topLayout          = QHBoxLayout()
         self.middleUpLayout     = QVBoxLayout()
         self.middleDownLayout   = QVBoxLayout()
-        self.bottomLayout     = QVBoxLayout()
-        self.topGroupBox        = QGroupBox()
-        self.bottomGroupBox   = QGroupBox()
+        self.bottomLayout       = QHBoxLayout()
+        ###################################################################################
+        ### Adding ChildLayouts to MainLayout                                    
+        ###################################################################################
+        self.mainLayout.addLayout(self.topLayout,30)
+        self.mainLayout.addLayout(self.middleUpLayout,20)
+        self.mainLayout.addLayout(self.middleDownLayout,20)
+        self.mainLayout.addLayout(self.bottomLayout,30)
+        ###################################################################################
+        ###### Add Widgets to Layouts                    
+        ###################################################################################
+        self.topLayout.addWidget(self.emptyWidgetTop)
+        self.middleUpLayout.addWidget(self.manageEmployeesBtn)
+        self.middleUpLayout.setContentsMargins(100,5,100,5)
+        self.middleDownLayout.addWidget(self.viewChartsBtn)
+        self.middleDownLayout.setContentsMargins(100,5,100,5)
+        self.bottomLayout.addWidget(self.emptyWidgetBottom)
+        ###################################################################################
+        ### Setting MainLayout                                 
+        ###################################################################################
+        self.setLayout(self.mainLayout)
+
+    def manageEmployee(self):
+        print("You clicked Manage Employees")
+
+    def viewCharts(self):
+        print("You clicked View Charts")
+
 
 def main():
     App = QApplication(sys.argv)
--- static/style.py
+++ static/style.py
@@ -1,5 +1,38 @@
 def mainWindowStyle():
     return """
-        background-color:#003A51;
+        background-color:#041F30;
         color: #E7F4FF;
+        font-family: 'Open Sans';
+    """
+def manageEmployeesBtnStyle():
+    return """
+        QPushButton {
+            background-color: #C93B9F;
+            color: #E7F4FF;
+            font-size: 14px;
+            font-weight: 600;
+            width: 100px;
+            height: 40px;
+            border-radius: 4px;
+        }
+        QPushButton:hover {
+            background-color: #ac3489;
+            color: #E7F4FF;
+        }
+    """
+def viewChartsBtnStyle():
+    return """
+        QPushButton {
+            background-color: #7E8FF3;
+            color: #E7F4FF;
+            font-size: 14px;
+            font-weight: 600;
+            width: 100px;
+            height: 40px;
+            border-radius: 4px;
+        }
+        QPushButton:hover {
+            background-color: #7985cb;
+            color: #E7F4FF;
+        }
     """
